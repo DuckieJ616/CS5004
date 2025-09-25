2d88e20c2e557d16187966a8ef505efabf21eed0
Xinyi Jiang <jiang.x2@northeastern.edu>
2025-09-25 16:17:53 -0700

    Lab2

---
+++ Labs/Lab2/javadocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"<Unnamed>","l":"Brand"},{"p":"<Unnamed>","l":"Color"},{"p":"<Unnamed>","l":"Kind"},{"p":"<Unnamed>","l":"Shoe"},{"l":"所有类和接口","u":"allclasses-index.html"}];updateSearchResults();
